92335d7aed6183d519eff1ed45b6716d59a8269a
achyusuf10 <jojogaul17@gmail.com>
2022-10-16 13:09:27 +0700

    update

--- lib/my_widget_package.dart
+++ lib/my_widget_package.dart
@@ -5,5 +5,6 @@ export './widgets/dialog/dialog_choice.dart';
 export './widgets/dialog/dialog_notif.dart';
 export './widgets/custom_text_form_field.dart';
 export './widgets/shimmer_widget.dart';
+export './widgets/circular_loading.dart';
 export './helper/no_scroll_glow_behavior.dart';
 export './data/enum.dart';
